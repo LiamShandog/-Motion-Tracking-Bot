4da168022165c96757d4ddf0fb0a020af9523260
Liam <liamshan13@gmail.com>
2025-11-17 15:15:04 -0500

    Updates in readme

---
+++ launch/motion_tracking_bot.launch.py
@@ -0,0 +1,36 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+
+def generate_launch_description():
+    pkg_name = 'motion_tracking_bot'
+
+    # Path to the YAML file
+    config = os.path.join(
+        get_package_share_directory(pkg_name),
+        'config',
+        'motion_tracking_bot.yaml'
+    )
+
+    ir_node = Node(
+        package=pkg_name,
+        executable='ir_sensor_node',
+        name='ir_sensor_node',
+        parameters=[config],
+        output='screen'
+    )
+
+    motion_node = Node(
+        package=pkg_name,
+        executable='speaker_node',
+        name='speaker_node',
+        parameters=[config],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        ir_node,
+        speaker_node,
+    ])
